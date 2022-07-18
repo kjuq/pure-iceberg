716e910dd7d81b2f3c446c0b80ac6105cf8cec43
kjuq <44694396+kjuq@users.noreply.github.com>
2022-07-18 11:09:45 +0900

    initial commit

---
+++ conf.d/pure_iceberg.fish
@@ -0,0 +1,28 @@
+# black     161821
+# red       e27878
+# green     b4be82
+# yellow    e2a478
+# blue      84a0c6
+# magenta   a093c7
+# cyan      89b8c2
+# white     c6c8d1
+# brblack   6b7089
+# brred     e98989
+# brgreen   c0ca8e
+# bryellow  e9b189
+# brblue    91acd1
+# brmagenta ada0d3
+# brcyan    95c4ce
+# brwhite   d2d4de
+
+set --universal pure_color_primary 84a0c6
+set --universal pure_color_info 89b8c2
+set --universal pure_color_mute 6b7089
+set --universal pure_color_success a093c7
+set --universal pure_color_normal normal
+set --universal pure_color_danger e27878
+set --universal pure_color_light c6c8d1
+set --universal pure_color_warning e2a478
+set --universal pure_color_dark 161821
+
+
